b395155e0cb64139d6f1f271800aa000c2b165d0
Jonathan Persaud <persaud.jon@gmail.com>
2018-03-13 10:42:56 -0400

    Add files via upload

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jonathan Persaud on 2/26/18.
+//  Copyright © 2018 Jonathan Persaud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
